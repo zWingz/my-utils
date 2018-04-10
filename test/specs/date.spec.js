f4716df8a53ba9173e13bcd3c89f726743b9f11c
zhengzhirong <zhengzhirong@youmi.net>
2018-04-10 10:25:06 +0800

    fix

--- test/specs/date.spec.js
+++ test/specs/date.spec.js
@@ -18,3 +18,31 @@ describe('DateFormat', () => {
     })
   })
   
+
+  describe('getMonthFirstAndEnd', () => {
+    const now = new Date('2018-02-23')
+    const {start, end} = dateUtils.getMonthFirstAndEnd({
+      offset: -1,
+      month: 2
+    })
+    it('first date is 1 and end date is 31', () => {
+      expect(start.getDate()).to.equal(1)
+      expect(end.getDate()).to.equal(31)
+    })
+  })
+
+  describe('getWeek', () => {
+    const now = new Date('2018-02-23')
+    const week = dateUtils.getWeek(now)
+    it('2018-02-23 is the 8th week in 2018', () => {
+      expect(week).to.equal(8)
+    })
+  })
+
+  describe('getWeekInYear', () => {
+    const now = new Date('2018-02-23')
+    const week = dateUtils.getWeekInYear(now)
+    it('2018-02-23 is the 8th week in 2018', () => {
+      expect(week).to.equal(8)
+    })
+  })
